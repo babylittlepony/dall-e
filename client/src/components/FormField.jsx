e2ebbf1fa1c01047d3f6a83a6baacbfcbc08c812
Egar <pramana.egar1540@gmail.com>
2023-02-10 16:34:23 +0700

    added tailwind prettier. created form field & working on create post page

--- client/src/components/FormField.jsx
+++ client/src/components/FormField.jsx
@@ -1,7 +1,48 @@
 import React from "react"
 
-const FormField = () => {
-  return <div>FormField</div>
+const FormField = ({
+  labelName,
+  type,
+  name,
+  placeholder,
+  value,
+  handleChange,
+  isSurpriseMe,
+  handleSurpriseMe,
+}) => {
+  return (
+    <div>
+      <div className="flex items-center gap-2 mb-2">
+        <label
+          htmlFor={name}
+          className="block text-sm font-medium text-gray-900"
+        >
+          {labelName}
+        </label>
+        {isSurpriseMe && (
+          <button
+            type="button"
+            onClick={handleSurpriseMe}
+            className="font-semibold text-xs bg-[#ececf1] p-2 rounded-[5px] text-not-that-black"
+          >
+            Surprise me
+          </button>
+        )}
+      </div>
+      <input
+        type={type}
+        id={name}
+        name={name}
+        placeholder={placeholder}
+        value={value}
+        onChange={handleChange}
+        required
+        className="bg-gray-50 border-gray-300 text-not-that-black text-sm rounded-lg focus:ring-[#4649ff] focus:border-black outline-none block w-full p-3 border "
+      />
+
+      <div className="relative bg-gray-50 border border-gray-300 text-not-that-black text-sm rounded-lg focus:ring-blue-500 focus:border-black w-64 p-3 h-64 justify-center flex items-center"></div>
+    </div>
+  )
 }
 
 export default FormField
--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -1,7 +1,59 @@
-import React from "react"
+import React, { useState } from "react"
+import { useNavigate } from "react-router-dom"
+
+import { preview } from "../assets"
+import { getRandomPrompt } from "../utils"
+import { FormField, Loader } from "../components"
 
 const CreatePost = () => {
-  return <div>CreatePost</div>
+  const navigate = useNavigate()
+  const [form, setForm] = useState({
+    name: "",
+    prompt: "",
+    photo: "",
+  })
+  const [generatingImg, setGeneratingImg] = useState(false)
+  const [loading, setLoading] = useState(false)
+
+  const handleSubmit = () => {}
+
+  const handleChange = (e) => {}
+  const handleSurpriseMe = () => {}
+
+  return (
+    <section className="max-w-7xl mx-auto">
+      <div className="font-bold text-not-that-black text-[32px]">
+        <h1>Create</h1>
+        <p className="mt-2 text-sky-blue text-[14px] max-w-[500px]">
+          Create imaginative and visually stunning images through{" "}
+          <span className="font-extrabold text-not-that-black">DALL-E AI</span>
+        </p>
+      </div>
+
+      <form className="mt-16 max-w-3xl" onSubmit={handleSubmit}>
+        <div className="flex flex-col gap-5">
+          <FormField
+            labelName="Your name"
+            type="text"
+            name="name"
+            placeholder="Fulan"
+            value={form.name}
+            handleChange={handleChange}
+          />
+          <FormField
+            labelName="Prompt"
+            type="text"
+            name="prompt"
+            placeholder="an oil painting by Matisse of a humanoid robot playing chess"
+            value={form.prompt}
+            handleChange={handleChange}
+            isSurpriseMe
+            handleSurpriseMe={handleSurpriseMe}
+          />
+        </div>
+      </form>
+    </section>
+  )
 }
 
 export default CreatePost
