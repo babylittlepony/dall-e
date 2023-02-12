5cfe221029ab44eec4c1e085b18c995eb87e9732
Egar <pramana.egar1540@gmail.com>
2023-02-12 14:48:48 +0700

    added cards img

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -12,6 +12,30 @@ const Card = ({ _id, name, prompt, photo }) => {
         alt={prompt}
         className="h-auto w-full rounded-xl object-cover"
       />
+
+      <div className="absolute bottom-0 left-0 right-0 m-2 hidden max-h-[94.5%] flex-col rounded-md bg-not-that-black p-4 group-hover:flex">
+        <p className="overflow-y-auto text-sm text-white">{prompt}</p>
+
+        <div className="mt-5 flex items-center justify-between gap-2">
+          <div className="flex items-center gap-2">
+            <div className="flex h-7 w-7 items-center justify-center rounded-full bg-purple-600 object-cover text-xs font-bold text-white">
+              {name[0]}
+            </div>
+            <p className="text-sm tracking-wider text-white">{name}</p>
+          </div>
+          <button
+            type="button"
+            onClick={() => downloadImage(_id, photo)}
+            className="border-none bg-transparent outline-none"
+          >
+            <img
+              src={download}
+              alt="download"
+              className="h-6 w-6 object-contain invert"
+            />
+          </button>
+        </div>
+      </div>
     </div>
   )
 }
