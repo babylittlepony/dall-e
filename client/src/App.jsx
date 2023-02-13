0b35f6eeb0e8f61fcedc860881defb5445b4c839
Egar <pramana.egar1540@gmail.com>
2023-02-13 09:16:16 +0700

    added about link

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,12 +11,20 @@ const App = () => {
         <Link to="/">
           <img src={logo} alt="logo" className="w-28 object-contain" />
         </Link>
-        <Link
-          to="/create-post"
-          className="rounded-md bg-black px-4 py-2 font-inter font-bold text-white"
-        >
-          Create
-        </Link>
+        <div>
+          <Link
+            to="/create-post"
+            className="rounded-md bg-black px-4 py-2 font-inter font-bold text-white"
+          >
+            Create
+          </Link>
+          <Link
+            to="https://openai.com/dall-e-2/"
+            className="ml-2 rounded-md bg-black px-4 py-2 font-inter font-bold text-white xs:ml-8"
+          >
+            About
+          </Link>
+        </div>
       </header>
       <main className="min-h-[calc(100vh-73px)] w-full bg-[#f9fafe] px-4 py-8 sm:p-8">
         <Routes>
